9dca3777e8424945b8c33f8712ec3b5cdfeb85cc
Matthew Araujo <matthewaraujo20@gmai.com>
2024-03-22 14:43:40 -0300

    feat: api content type xml or json

--- api/main.go
+++ api/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"net/http"
 )
 
 type Customer struct {
-	Name string `json:"name"`
-	City string `json:"city"`
-	Zip  string `json:"zip"`
+	Name string `json:"name" xml:"name"`
+	City string `json:"city" xml:"city"`
+	Zip  string `json:"zip" xml:"zip"`
 }
 
 func main() {
@@ -30,7 +31,12 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{Name: "Jane Doe", City: "San Francisco", Zip: "94105"},
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Set("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customers)
 }
