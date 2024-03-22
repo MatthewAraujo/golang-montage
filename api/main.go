af88f9bf3b245f7f39780f095f40a05e59060975
Matthew Araujo <matthewaraujo20@gmai.com>
2024-03-22 14:37:14 -0300

    feat: api Json encoder

--- api/main.go
+++ api/main.go
@@ -1,15 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Customer struct {
+	Name string `json:"name"`
+	City string `json:"city"`
+	Zip  string `json:"zip"`
+}
 
 func main() {
 
 	//defines routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "John Doe", City: "San Francisco", Zip: "94105"},
+		{Name: "Jane Doe", City: "San Francisco", Zip: "94105"},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
